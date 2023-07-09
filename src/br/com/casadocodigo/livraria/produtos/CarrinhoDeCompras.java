c871791fc2ede3ef5b728085880308403a42ce2b
Ronaldo Gregorio <gr3goriowr@gmail.com>
2023-07-09 13:03:51 -0300

    Atualizando o carrinho de compras

--- src/br/com/casadocodigo/livraria/produtos/CarrinhoDeCompras.java
+++ src/br/com/casadocodigo/livraria/produtos/CarrinhoDeCompras.java
@@ -3,10 +3,14 @@ package br.com.casadocodigo.livraria.produtos;
 public class CarrinhoDeCompras extends Livro{
 
     private double total;
+    private Produto[] produtos = new Produto[10];
+    private int contador = 0;
 
     public void adiciona(Produto produto){
 
         System.out.println("Adicionando: " + produto);
+        this.produtos[contador] = produto;
+        contador++;
         total += produto.getValor();
     }
 
