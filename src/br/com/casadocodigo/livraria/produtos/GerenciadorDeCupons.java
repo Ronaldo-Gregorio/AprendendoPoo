55d6401142a9567676a360bc88d0df98d3dbde4f
Ronaldo Gregorio <gr3goriowr@gmail.com>
2023-07-11 12:53:04 -0300

    Melhorando o gerenciador de descontos com hashmap

--- src/br/com/casadocodigo/livraria/produtos/GerenciadorDeCupons.java
+++ src/br/com/casadocodigo/livraria/produtos/GerenciadorDeCupons.java
@@ -1,18 +1,22 @@
 package br.com.casadocodigo.livraria.produtos;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GerenciadorDeCupons {
-    private Set<String> cupons;
+
+    private Map<String, Double> cupons;
 
     public GerenciadorDeCupons(){
-        this.cupons = new HashSet<String>();
+        this.cupons = new HashMap<>();
 
-        cupons.addAll(Arrays.asList("CUP74", "CUP158", "CUP14", "CUP52", "CUP21", "CUP221", "CUP91", "CUP327", "CUP410", "CUP275", "CUP 484", "CUP207", "CUP96", "CUP119", "CUP174", "CUP291", "CUP1", "CUP115", "CUP222", "CUP272"));
+        cupons.put("CUP74", 10.0);
+        cupons.put("CUP158", 15.0);
+        cupons.put("CUP14", 5.99);
+        cupons.put("CUP52", 20.0);
+
     }
 
-    public boolean validaCupom(String cupom){
-        return this.cupons.contains(cupom);
+    public Double validaCupom(String cupom){
+        return this.cupons.get(cupom);
     }
 }
--- src/br/com/casadocodigo/livraria/teste/ConsultaDeDescontos.java
+++ src/br/com/casadocodigo/livraria/teste/ConsultaDeDescontos.java
@@ -4,9 +4,13 @@ import br.com.casadocodigo.livraria.produtos.GerenciadorDeCupons;
 public class ConsultaDeDescontos {
 
     public static void main(String[] args) {
-        GerenciadorDeCupons gerenciador = new GerenciadorDeCupons(); 
-        if(gerenciador.validaCupom("CUP1234")){
+
+        GerenciadorDeCupons gerenciador = new GerenciadorDeCupons();
+        
+        Double desconto = gerenciador.validaCupom("CUP74"); 
+        if(desconto != null){
             System.out.println("Cupom de desconto válido");
+            System.out.println("Valor de desconto: " + desconto);
         }else{
             System.out.println("Esse cupom não existe");
         }
